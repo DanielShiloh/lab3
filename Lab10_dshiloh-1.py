7f2862f90c52d09fa9e7c8167e8773421535360a
Daniel Shiloh <dshiloh@cscc.edu>
2026-06-24 22:38:23 -0400

    Wrote out main logic

--- Lab10_dshiloh-1.py
+++ Lab10_dshiloh-1.py
@@ -34,3 +34,32 @@ def main():
     intro += "3. Tarzan\n"
     intro += "4. Princess Mars\n"
     intro += "5. Exit\n"
+
+    while True:
+
+        print(intro)
+        choice = input("Enter your choice (1-5): ")
+
+        try:
+            choice = int(choice)
+            #find path for that story
+        except ValueError:
+            #print needs to be a number 1-5
+            continue
+        except FileNotFoundError:
+            #print invalid input
+            continue
+
+        choice_path = 0
+        choice_title = ""
+        #if 1, monte_cristo.txt...
+
+        print(f"\nProcessing {choice_title}...")
+
+        #instance of WordAnalyzer
+
+        #process_file()
+
+        #if successful, print_report()
+
+        print("Press Enter to return to the menu...")
