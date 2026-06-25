a676dfdb19dcc270d8e791246320c2809362ff23
Daniel Shiloh <dshiloh@cscc.edu>
2026-06-24 22:22:20 -0400

    Added text files to read

--- Lab10_dshiloh-1.py
+++ Lab10_dshiloh-1.py
@@ -1,1 +1,36 @@
-#the
+"""
+Word Count
+Daniel Shiloh
+Counts the frequency of every word in a txt file, prints an alphabetical report
+June 24, 2026
+"""
+
+from pathlib import Path
+
+class WordAnalyzer:
+    """
+    """
+
+    def __init__(self, filepath):
+        """
+        """
+        #store filepath as private pathlibrary
+    
+    def process_file(self):
+        """
+        """
+    
+    def print_report(self):
+        """
+        """
+
+def main():
+    """
+    """
+    intro = "--- Word Analyzer ---\n"
+    intro += "Please select a file to analyze:\n"
+    intro += "1. Monte Cristo\n"
+    intro += "2. Treasure Island\n"
+    intro += "3. Tarzan\n"
+    intro += "4. Princess Mars\n"
+    intro += "5. Exit\n"
