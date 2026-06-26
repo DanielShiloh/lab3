70766ffc63c9aab4fa6b3e48d61e4f61e9db940a
Daniel Shiloh <dshiloh@cscc.edu>
2026-06-25 22:00:50 -0400

    Added counting of words in file

--- Lab10_dshiloh-1.py
+++ Lab10_dshiloh-1.py
@@ -14,16 +14,52 @@ class WordAnalyzer:
 
     def __init__(self, filepath):
         """
+        Args: filepath: str, path of txt file
+        Create dictionary for word frequencies
         """
-        #store filepath as private pathlibrary
+        self.filepath = Path(filepath)
+        self.frequencies = {}
     
     def process_file(self):
         """
+        Check that the file path exists,
+        remove punctuation,
+        read line by line and update frequencies
         """
+        try:
+            if not self.filepath.exists():
+                raise FileNotFoundError
+        
+            translator = str.maketrans('', '', string.punctuation)
+
+            with self.filepath.open as file:
+                for line in file:
+                    plain_line = line.translate(translator).lower()
+                    words = plain_line.split()
+                    for word in words:
+                        self.frequencies[word] += 1
+            return True
+        
+        except FileNotFoundError:
+            print(f"{self.filepath} could not be found.")
+            return False
+
     
     def print_report(self):
         """
+        Sort words, print with frequencies
+        Use length of longest word for column alignment
         """
+        sorted_words = sorted(self.frequencies.keys())
+
+        if not sorted_words:
+            print("No words found.")
+            return
+        
+        max_len = max(len(word) for word in sorted_words)
+
+        for word in sorted_words:
+            print(f"{word:<{max_len}} :: {self.frequencies[word]}")
 
 def main():
     """
@@ -49,12 +85,15 @@ def main():
         
         choice = input("Enter your choice (1-5): ")
 
+        if choice == 5:
+            print("Goodbye!")
+            break
+    
         try:
             choice = int(choice)
             selected_path = file_options[choice]
-            #find path for that story
         except ValueError:
-            print("Invalid choice.  Please type a number, 1-5.")
+            print("Invalid choice.  Please type a number.")
             continue
         except FileNotFoundError:
             print("Invalid choice.  Please select from 1-5.")
@@ -62,13 +101,16 @@ def main():
 
         print(f"\nProcessing {file_options[choice]}...")
 
-
         #instance of WordAnalyzer
 
         #process_file()
 
         #if successful, print_report()
-
+        
+        again = ""
         again = input("Press Enter to return to the menu...")
         if again == "":
             break
+
+if __name__ == "__main__":
+    main()
